bb5c9f90375e53a88c966d8b7c3b0fc2c49b79e5
elarion83 <gruwe.nicolas@hotmail.fr>
2020-07-03 21:11:32 +0200

    it's fucking working

--- __init__.py
+++ __init__.py
@@ -69,14 +69,14 @@ class YoutubeSkill(CommonPlaySkill):
     # Attempt to find the first result matching the query string
     def search_youtube(self, search_term):
         tracklist = []
-        self.vid_url ='/watch?v=bWXazVhlyxQ'
+        self.vid_url ='/watch?v=5qap5aO4i9A'
         self.stream_url = self.get_stream_url(self.vid_url)
         LOG.debug('Found stream URL: ' + self.vid_url)
-        LOG.debug('Media title: ' + 'chill nigger')
+        LOG.debug('Media title: ' + 'Chill Music')
         tracklist.append(self.stream_url)
         self.mediaplayer.add_list(tracklist)
         self.audio_state = 'playing'
-        self.speak_dialog('now.playing', {'content': 'chill nigger'} )
+        self.speak_dialog('now.playing', {'content': 'Chill Music'} )
         wait_while_speaking()
         self.mediaplayer.play()
 
