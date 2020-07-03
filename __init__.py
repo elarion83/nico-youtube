d57bb17299d40f6fc770edc11f1f2f841de99568
elarion83 <gruwe.nicolas@hotmail.fr>
2020-07-03 19:57:31 +0200

    debug

--- __init__.py
+++ __init__.py
@@ -68,18 +68,7 @@ class YoutubeSkill(CommonPlaySkill):
 
     # Attempt to find the first result matching the query string
     def search_youtube(self, search_term):
-        tracklist = []
-        res = requests.get(search_url + search_term)
-        # TODO: check status code etc...
-        html = res.content
-        soup = BeautifulSoup(html, 'html.parser')
-        vids = soup.findAll(attrs={'class':'yt-uix-tile-link'})
-
-        for vid in vids:
-            if not re.match('/watch\?v=\w{11}', vid['href']):
-              LOG.debug('no media: ' + vid['href'])
-              continue
-
+  
             self.vid_url = 'https://www.youtube.com/watch?v=5qap5aO4i9A'
             self.vid_name = vid.string
             self.stream_url = self.get_stream_url(self.vid_url)
@@ -91,13 +80,7 @@ class YoutubeSkill(CommonPlaySkill):
             self.speak_dialog('now.playing', {'content': self.vid_name} )
             wait_while_speaking()
             self.mediaplayer.play()
-            return
-
-        # We didn't find any playable results
-        self.speak_dialog('not.found')
-        wait_while_speaking()
-        LOG.debug('Could not find any results with the query term: ' + search_term)
-
+  
     def get_stream_url(self, youtube_url):
         abs_url = base_url + youtube_url
         LOG.debug('pafy processing: ' + abs_url)
