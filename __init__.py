3f805652e3817c69b22867832b2d0fadafa98b8a
elarion83 <gruwe.nicolas@hotmail.fr>
2020-07-07 20:34:59 +0200

    try title

--- __init__.py
+++ __init__.py
@@ -74,7 +74,7 @@ class YoutubeSkill(CommonPlaySkill):
         self.vid_url = random.choice(urls_for_rand_video)
         self.stream_url = self.get_stream_url(self.vid_url)
         LOG.debug('Found stream URL: ' + self.vid_url)
-        LOG.debug('Media title: ' + 'Chill Music')
+        LOG.debug('Media title: ' + self.vid_title)
         tracklist.append(self.stream_url)
         self.mediaplayer.add_list(tracklist)
         self.audio_state = 'playing'
