362485291133912e5d6966796f7166436b287633
elarion83 <gruwe.nicolas@hotmail.fr>
2020-07-08 19:39:00 +0200

    yeaaaah fucking working brooo

--- __init__.py
+++ __init__.py
@@ -82,11 +82,10 @@ class YoutubeSkill(CommonPlaySkill):
             urls_for_rand_video = ['/watch?v=tAGnKpE4NCI']
             LOG.debug('ROCK')
         else :
-            urls_for_rand_video = ['/watch?v=vSkb0kDacjs']
+            urls_for_rand_video = ['/watch?v=vSkb0kDacjs', '/watch?v=lB8uQ_zO-o8','/watch?v=HPssThWONWk', '/watch?v=VGrNjY6_x2k','/watch?v=f6QFfmwFT3k','/watch?v=OvYX6IM8ME4','/watch?v=FspennuvEoY', '/watch?v=Tqj2bJGRhNI','/watch?v=_0VVXhcsTdo','/watch?v=AaxFuXpcQmE', '/watch?v=YK0D4byEVH4', '/watch?v=qd_8mm906GA']
             LOG.debug('OTHEEEER')
 
         tracklist = []
-       # urls_for_rand_video = ['/watch?v=lB8uQ_zO-o8','/watch?v=HPssThWONWk', '/watch?v=VGrNjY6_x2k','/watch?v=f6QFfmwFT3k','/watch?v=OvYX6IM8ME4','/watch?v=FspennuvEoY', '/watch?v=Tqj2bJGRhNI','/watch?v=_0VVXhcsTdo','/watch?v=AaxFuXpcQmE', '/watch?v=YK0D4byEVH4', '/watch?v=qd_8mm906GA']
         self.vid_url = random.choice(urls_for_rand_video)
         self.stream_url = self.get_stream_url(self.vid_url)
         LOG.debug('Found stream URL: ' + self.vid_url)
